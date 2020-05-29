4c496b811c1a57e958aa05dbae41a7394cba7c9e
Jeffrey Lan <jtlan.sf@gmail.com>
2020-05-29 14:15:48 -0400

    parses bitter almond text into array; ignores sweet almond text, incomplete parsing of other spices

--- spice-crawler.rb
+++ spice-crawler.rb
@@ -10,16 +10,21 @@ spice_data=JSON.load(spices)
 url=spice_data[78]['url']
 page=Nokogiri::HTML(open(url))
 
+# parse common name from header
 common_name=page.css('a[name="top"]').text.split('(')[0]
 puts "Common name: #{common_name}"
 
+# parse latin name from header
 latin_name=page.css('i[lang="la"]').first.text
 puts "Latin name: #{latin_name}"
 
-part_used=page.css('a[name="part"] p.dd')
-# [1].text[1..-1]
-puts "Part used: #{part_used}"
+# part_used=page.css('a[name="part"] p.dd')
+# # [1].text[1..-1]
+# puts "Part used: #{part_used}"
 
-info_arr=page.css('p.dd').text.split('\n')
-puts JSON.pretty_generate(info_arr)
+info_arr=page.css('dd').map{|p| p=p.text[1..-2].to_s.gsub!("\n"," ")}[4..-2]
+# .split("\n").filter{|el| el.length>1}
+File.open('info_arr.json','w') << JSON.pretty_generate(info_arr)
+# {|json| json << JSON.pretty_generate(info_arr)}
+# puts JSON.pretty_generate(info_arr)
 #.select {|info| info.length>3}
