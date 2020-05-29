1bba9cd1ec7c8294b4ca3287636b07af902de961
Jeffrey Lan <jtlan.sf@gmail.com>
2020-05-29 14:56:40 -0400

    exports JSON with headers and info; still need to remove image captions from text

--- spice-crawler.rb
+++ spice-crawler.rb
@@ -7,24 +7,26 @@ spices=File.open('./spice-index.json')
 spice_data=JSON.load(spices)
 
 # get url from first object in spice_data array and open in Nokogiri
-url=spice_data[78]['url']
+url=spice_data[59]['url']
 page=Nokogiri::HTML(open(url))
 
 # parse common name from header
 common_name=page.css('a[name="top"]').text.split('(')[0]
-puts "Common name: #{common_name}"
 
 # parse latin name from header
 latin_name=page.css('i[lang="la"]').first.text
-puts "Latin name: #{latin_name}"
 
-# part_used=page.css('a[name="part"] p.dd')
-# # [1].text[1..-1]
-# puts "Part used: #{part_used}"
+# create a hash with common and latin names
+spice_hash={:"common-name"=>common_name,:"latin-name"=>latin_name}
 
-info_arr=page.css('dd').map{|p| p=p.text[1..-2].to_s.gsub!("\n"," ")}[4..-2]
-# .split("\n").filter{|el| el.length>1}
-File.open('info_arr.json','w') << JSON.pretty_generate(info_arr)
-# {|json| json << JSON.pretty_generate(info_arr)}
-# puts JSON.pretty_generate(info_arr)
-#.select {|info| info.length>3}
+# get an array of strings from section headings
+section_arr=page.css('dt b').map{|sect| sect.text.to_s.downcase.gsub!(/[\s \u00a0]/, "-")}[2..-2]
+
+# get an array of strings from informational paragraphs
+info_arr=page.css('dd').map{|p| p.text[1..-1].to_s.gsub!("\n"," ")}[2..-2]
+
+# create a hash with section_arr as keys and info_arr as values
+spice_hash.merge!(section_arr.zip(info_arr).to_h)
+
+# export to JSON
+File.open('info_arr.json','w') << JSON.pretty_generate(spice_hash)
