07dcf7121adfa791d0560822552b5f858d755b4e
Jeffrey Lan <jtlan.sf@gmail.com>
2020-05-31 13:49:35 -0400

    refactored text parsing to downcase_kebab()

--- spice_crawler.rb
+++ spice_crawler.rb
@@ -7,7 +7,7 @@ spices=File.open('./spice-index.json')
 spice_data=JSON.load(spices)
 
 # get url from first object in spice_data array and open in Nokogiri
-url=spice_data[16]['url']
+url=spice_data[89]['url']
 page=Nokogiri::HTML(open(url))
 
 # parse common name from header
@@ -19,10 +19,17 @@ latin_name=page.css('i[lang="la"]').first.text
 # create a hash with common and latin names
 spice_hash={:"common-name"=>common_name,:"latin-name"=>latin_name}
 
-# get an array of strings from section headings
-section_arr=page.css('dt b').map{|sect| sect.text.downcase}[0..-2]
+# reformat text casing
+def downcase_kebab(label)
+	label.split(/[\s \u00a0]/).length==1?
+		label.downcase : label.downcase.gsub!(/[\s \u00a0]/, "-")
+	#  label.downcase.gsub!(/[\s \u00a0]/, "-")
+end
 
-section_arr.map{|sect| sect.gsub!(/[\s \u00a0]/, "-")}
+# get an array of strings from section headings
+section_arr=page.css('dt b').map{|sect| downcase_kebab(sect.text)}[0..-2]
+
+# section_arr.map{|sect| sect}
 
 # get an array of strings from informational paragraphs
 info_arr=page.css('dd').map{|p| p.text[1..-1].gsub!(/[\n]/," ")}[0..-2]
@@ -31,4 +38,4 @@ info_arr=page.css('dd').map{|p| p.text[1..-1].gsub!(/[\n]/," ")}[0..-2]
 spice_hash.merge!(section_arr.zip(info_arr).to_h)
 
 # export to JSON
-File.open('info_arr.json','w') << JSON.pretty_generate(spice_hash)
+File.open("#{downcase_kebab(spice_hash[:'common-name'])}-info_arr.json",'w') << JSON.pretty_generate(spice_hash)
