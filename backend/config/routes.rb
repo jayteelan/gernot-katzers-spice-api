7833b13f9053fbdbc0e185559f84bd15d91f3fd9
Jeffrey Lan <jtlan.sf@gmail.com>
2020-06-06 14:26:45 -0400

    ready for seed data

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :spice_constituents
-  resources :parts
-  resources :images
-  resources :origins
-  resources :constituents
-  resources :families
-  resources :spices
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+	ActiveAdmin.routes(self)
+	
+	scope '/api' do
+		# resources :spice_constituents
+		resources :parts
+		# resources :images
+		resources :origins
+		resources :constituents
+		resources :families
+		resources :spices
+	end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
